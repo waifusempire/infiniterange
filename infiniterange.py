c8b50e6e0fe65f260ffd5139ccbff81d17e639b7
_waifus_empire_ <82737350+waifusempire@users.noreply.github.com>
2022-10-24 12:26:34 +0200

    Add files via upload

--- infiniterange.py
+++ infiniterange.py
@@ -1,15 +1,17 @@
 class infinite_range:
     def __init__(self, start: int = 0, step: int = 1):
+        """start: optional<int> = default<0>
+step: optional<int> = default<1>"""
         self.__start = start
         self.__step = step
-        self._current = start
+        self.__current = start
 
     def __iter__(self):
         return self
     
     def __next__(self):
-        val = self._current
-        self._current += self.__step
+        val = self.__current
+        self.__current += self.__step
         return val
     
     def __repr__(self):
@@ -17,3 +19,24 @@ class infinite_range:
     
     def __str__(self):
         return f"<{type(self).__name__} start={self.__start} step={self.__step}>"
+    
+    def reset(self):
+        "Reset the range to the start value"
+        self.__current = self.__start
+
+    @property
+    def current(self):
+        "The current value"
+        return self.__current
+    
+    @property
+    def start(self):
+        "The start value"
+        return self.__start
+    
+    @property
+    def step(self):
+        "How many values should the range skip between iterations"
+        return self.__step
+    
+
